77618a9fc5c0841b45a3d1d141a74e50bd417602
x3Michels <x3michels@gmail.com>
2023-10-18 11:40:30 -0300

    v2 rework

--- script.js
+++ script.js
@@ -50,16 +50,16 @@ function showButtonContent(menuIndex, buttonIndex) {
                 case 1:
                     video.src = './assets/heros/hero2/2pangolier_shield_crash.webm';
                     break;
-                case 0:
+                case 2:
                     video.src = './assets/heros/hero2/3pangolier_lucky_shot.webm';
                     break;
-                case 1:
+                case 3:
                     video.src = './assets/heros/hero2/4pangolier_gyroshell.webm';
                     break;
-                case 0:
+                case 4:
                     video.src = './assets/heros/hero2/5pangolier_aghanims_shard.webm';
                     break;
-                case 1:
+                case 5:
                     video.src = './assets/heros/hero2/6pangolier_aghanims_scepter.webm';
                     break;
             }
@@ -72,16 +72,16 @@ function showButtonContent(menuIndex, buttonIndex) {
                 case 1:
                     video.src = './assets/heros/hero3/2rubick_fade_bolt.webm';
                     break;
-                case 0:
+                case 2:
                     video.src = './assets/heros/hero3/3rubick_arcane_supremacy.webm';
                     break;
-                case 1:
+                case 3:
                     video.src = './assets/heros/hero3/4rubick_spell_steal.webm';
                     break;
-                case 0:
+                case 4:
                     video.src = './assets/heros/hero3/5rubick_aghanims_shard.webm';
                     break;
-                case 1:
+                case 5:
                     video.src = './assets/heros/hero3/6rubick_aghanims_scepter.webm';
                     break;
             }
